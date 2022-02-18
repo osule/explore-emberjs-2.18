ff950921be19f724e5d5b4f387fbb8ee1ad8bc4c
Oluwafemi Sule <oluwafemisule@outlook.com>
2022-02-18 16:51:53 +0100

    Create template helper for classifying rental property type

---
+++ tests/integration/helpers/rental-property-type-test.js
@@ -0,0 +1,22 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('rental-property-type', 'helper:rental-property-type', {
+  integration: true
+});
+
+test('it renders correctly for a Standalone rental', function(assert) {
+  this.set('inputValue', 'Estate');
+
+  this.render(hbs`{{rental-property-type inputValue}}`);
+
+  assert.equal(this.$().text().trim(), 'Standalone');
+});
+
+test('it renders correctly for a Community rental', function(assert) {
+  this.set('inputValue', 'Apartment');
+
+  this.render(hbs`{{rental-property-type inputValue}}`);
+
+  assert.equal(this.$().text().trim(), 'Community');
+});
